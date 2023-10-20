6f12a0a6fb6517f3f9768cb80aee53c06e00c8a1
Udhaymkd07 <udhayudhay.mkd@gmail.com>
2023-10-20 15:06:44 +0530

    udhay's new code

--- src/com/git/demo/Demo.java
+++ src/com/git/demo/Demo.java
@@ -3,6 +3,9 @@ package com.git.demo;
 public class Demo {
 	public static void main(String[] args) {
 		System.out.println("This is Git Demo");
+		System.out.println("Hi iam udhay");
+		System.out.println("Hi this is udhay's first project");
+		System.out.println("This is my sample code");
 	}
 
 }
